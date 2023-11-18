8e7458b836eb463bc6a26298cdc3c315829ad96f
jeetch <jeetch96@gmail.com>
2023-11-19 02:39:18 +0530

    new game

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,11 +1,16 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function GET(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const endpoint = req.query.kindeAuth;
-  const handler = await handleAuth(req, endpoint);
-  return handler(req, res);
+  if (req.method === 'GET') {
+    const endpoint = req.query.kindeAuth;
+    const handler = await handleAuth(req, endpoint);
+    return handler(req, res);
+  }
+
+  // Handle other request methods if necessary
+  res.status(405).send('Method Not Allowed');
 }
